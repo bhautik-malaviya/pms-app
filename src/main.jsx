379a8fc4eddb028b4a5bdd0b32efa76351dc1214
Bhautik Malaviya <bh4utik@gmail.com>
2026-04-23 17:57:29 +0530

    Remove StrictMode wrapper from render method

--- src/main.jsx
+++ src/main.jsx
@@ -7,11 +7,9 @@ import App from './App.jsx';
 import './index.css';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <App />
       </PersistGate>
     </Provider>
-  </StrictMode>,
 );
